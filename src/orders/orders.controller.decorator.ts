5eb3b00d2feda2dee31f77f4203c8f93943e50b9
choidoorim <doorim321@naver.com>
2022-05-09 02:01:55 +0900

    feat: swagger body

--- src/orders/orders.controller.decorator.ts
+++ src/orders/orders.controller.decorator.ts
@@ -3,6 +3,7 @@ import { ApiTags } from '@nestjs/swagger';
 
 import { JwtTokenAuth } from '@app/utils/guard';
 import { ApiDoc } from '@app/config/decorator';
+import { CreateOrderRequestDto } from './dto';
 
 export const OrdersController = () =>
   applyDecorators(Controller({ path: 'orders' }), ApiTags('orders'));
@@ -13,5 +14,6 @@ export const CreateOrder = () =>
     JwtTokenAuth(),
     ApiDoc({
       summary: '상품 주문 API',
+      bodyOptions: { type: CreateOrderRequestDto },
     }),
   );
